a4414e9419e669ee0318fdb388e46b5d81f1bbcb
devJamesP <znzldh1234@gmail.com>
2021-09-16 08:54:05 +0900

    공통 모듈 분리

--- fastcampus_web_package_heropy/part2_vcs_git_basic/starbucks/js/main.js
+++ fastcampus_web_package_heropy/part2_vcs_git_basic/starbucks/js/main.js
@@ -1,23 +1,4 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
 
-searchEl.addEventListener('click', function () {
-  // Click Event Handler Logic
-  searchInputEl.focus();
-});
-
-
-searchInputEl.addEventListener('focus', function () {
-  // Focus Event Handler Logic
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-  // Blur Event Handler Logic
-  searchEl.classList.remove('focused');
-  searchInputEl.removeAttribute('placeholder')
-});
 
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -81,9 +62,9 @@ new Swiper('.promotion .swiper-container', {
   spaceBetween: 10, // 슬라이드 사이 여백
   centeredSlides: true, // 1번 슬라이드가 가운데 보이기
   loop: true,
-  // autoplay:  {
-  //   delay: 5000,
-  // }
+  autoplay:  {
+    delay: 5000,
+  },
   pagination: {
     el: '.promotion .swiper-pagination', // 사용자의 페이지 요소 선택자
     clickable: true // 사용자의 페이지 번호 요소 제어
@@ -157,9 +138,3 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-
-// 날짜 자동 계산
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); // 2021반환
-
